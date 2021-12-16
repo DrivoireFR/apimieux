e4a30508ac08bcbd4ce132b39eb7c793e44c5cb5
DrivoireFR <54139583+DrivoireFR@users.noreply.github.com>
2021-12-16 15:52:27 +0100

    setup axios login and get users

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/css/index.css'
+import Axios from 'axios'
 
+const axios = Axios.create()
+axios.defaults.baseURL = 'http://localhost:8080'
 
 const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
+
 app.use(router)
 app.mount('#app')
 app.use(store)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,12 +7,12 @@
         <Card class="w-full max-w-md">
             <label for="mail-input" class="text-lg text-white">
                 Email
-                <input id="mail-input" type="text" class="text-emerald-400 bg-white rounded-sm p-4 w-full mb-8">
+                <input id="mail-input" v-model="form.username" type="text" class="text-emerald-400 bg-white rounded-sm p-4 w-full mb-8">
             </label>
 
             <label for="mail-input" class="text-lg text-white">
                 Password
-                <input id="mail-input" type="text" class="text-emerald-400 bg-white rounded-sm p-4 w-full mb-8">
+                <input id="mail-input" v-model="form.password" type="text" class="text-emerald-400 bg-white rounded-sm p-4 w-full mb-8">
             </label>
 
             <input class="p-4 bg-white rounded-md text-emerald-400 shadow-md cursor-pointer hover:text-white hover:bg-emerald-300 transition-all" type="submit" value="Se connecter" @click="login">
@@ -34,13 +34,21 @@ export default {
     },
     data () {
         return {
-
+            form: {
+                username: '',
+                password: ''
+            }
         }
     },
     methods: {
-        login () {
-            console.log('login')
-            this.$store.commit('login')
+        async login () {
+            const login = await this.$axios.post('/api/services/controller/user/login', this.form)
+            this.$axios.defaults.headers.common['Authorization'] = login.data
+            
+            
+            const getUsers = await this.$axios.get('/users')
+            console.log(getUsers)
+        
         }
     }
 }
